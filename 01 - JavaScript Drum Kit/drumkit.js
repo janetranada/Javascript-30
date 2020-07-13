3c05370f13c11388eaa968147734a85b07f9ca11
janetranada <jyranada@gmail.com>
2020-07-13 15:12:19 +0300

    add mouse click event for sound

--- 01 - JavaScript Drum Kit/drumkit.js
+++ 01 - JavaScript Drum Kit/drumkit.js
@@ -1,8 +1,18 @@
 
-function playSound(e) {
+function playSoundFromKeypress(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    
+    playSound(audio, key);
+}
+
+function playSoundFromClick(e) {    
+    const attributeKey = e.currentTarget.getAttribute('data-key');
+    const audio = document.querySelector(`audio[data-key="${attributeKey}"]`);
+    const key = document.querySelector(`.key[data-key="${attributeKey}"]`);
+    playSound(audio, key);
+}
+
+function playSound (audio, key) {
     if (!audio) return;
     
     audio.currentTime = 0;
@@ -17,5 +27,6 @@ function removeTransition(e) {
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+keys.forEach(key => key.addEventListener('click', playSoundFromClick))
 
-window.addEventListener('keydown', playSound);
+window.addEventListener('keydown', playSoundFromKeypress);
